bb9f568ccc6ec42d5c73f58c4e6da9ea318af9aa
dolunaysuratusagi <baglanebru@gmail.com>
2021-12-18 08:08:14 +0100

    FINALMENTE!!!!

--- src/00_controller.cpp
+++ src/00_controller.cpp
@@ -3,12 +3,17 @@
 #include "geometry_msgs/Twist.h"
 #include "second_assignment/UserInput.h"
 #include "std_srvs/Empty.h"
-#include "second_assignment/vel.h"
+
+ros::ServiceServer vel_service;
 
 ros::Publisher pub;
 
-float uservel = 0.0;
+float vel_min = -3.5; // without declaring min, robot can turn back after some decrements :D
 
+float vel_now = 1.0; // initial velocity change value
+float inc_dec = 0.5; // increment-decrement value
+
+// subsectioning and taking the min
 float subsections(int start, int end, float dist[])
 {
     int min;
@@ -16,7 +21,7 @@ float subsections(int start, int end, float dist[])
     float subs[size];
     for (int i = 0; i < size; i++)
     {
-        subs[i] = dist[i+start];
+        subs[i] = dist[i + start];
     }
     min = subs[0];
     for (int i = 0; i < size; i++)
@@ -32,7 +37,7 @@ void baseCallback(const sensor_msgs::LaserScan::ConstPtr &msg) // constant point
     int i;
     int size = msg->ranges.size();
     char user_req;
-    float dist[size], min1, min2, min3, min4, min5;
+    float dist[size], min2, min3, min4;
     float left_bottom[91], left_mid[180], front[180], right_mid[180], right_bottom[90];
 
     second_assignment::UserInput us_inp;
@@ -44,13 +49,13 @@ void baseCallback(const sensor_msgs::LaserScan::ConstPtr &msg) // constant point
         dist[i] = msg->ranges[i];
 
     // Subsectioning hint is used, here are the subsections' mins.
-    min2 = subsections(91, 270, dist);      // left_mid 180
-    min3 = subsections(271, 450, dist);        // front 180
-    min4 = subsections(451, 630, dist);    // right_mid 180
+    min2 = subsections(91, 270, dist);  // left_mid 180
+    min3 = subsections(271, 450, dist); // front 180
+    min4 = subsections(451, 630, dist); // right_mid 180
 
     // if front is close, stop first
     if (min3 < 0.5)
-    {                          // 1.1003 returned at second... ///1.0956 worked usuallybut...
+    { // 1.1003 returned at second... ///1.0956 worked usuallybut...
         my_vel.linear.x = 0.5;
         my_vel.linear.y = 0.5;
 
@@ -70,17 +75,36 @@ void baseCallback(const sensor_msgs::LaserScan::ConstPtr &msg) // constant point
     }
     else
     {
-        my_vel.linear.x = 3.5 + uservel;    // user's wish have been realized. user feels acknowledged. user is happy.
-        my_vel.linear.y = 0.0;              // https://www.youtube.com/watch?v=dU6EMlfjPFA 
+        my_vel.linear.x = 3.5 + vel_now; // user's wish have been realized. user feels acknowledged. user is happy.
+        my_vel.linear.y = 0.0;           // https://www.youtube.com/watch?v=dU6EMlfjPFA
         my_vel.angular.z = 0.0;
         pub.publish(my_vel);
     }
 }
 
-void velCallback(const second_assignment::vel::ConstPtr &msg)
-// to get user input from the topic
+bool serviceCallback(second_assignment::UserInput::Request &req, second_assignment::UserInput::Response &res)
 {
-    uservel = msg->velnow;
+    switch (req.userinp) // request is evaluated.
+    {
+    case 'x':
+    case 'X':
+        /* increase command */
+        vel_now = vel_now + inc_dec;
+        break;
+    case 'z':
+    case 'Z':
+        /* decrease command */
+        if (vel_now > vel_min)
+            vel_now = vel_now - inc_dec;
+        else
+            printf("Sorry you are already too slow, honey.\n");
+        break;
+    default:
+        vel_now = vel_now;
+        break;
+    }
+    res.uservel = vel_now;
+    return true;
 }
 
 int main(int argc, char **argv)
@@ -89,9 +113,11 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "controller");
     ros::NodeHandle nh;
 
-    /* /base_scan(from robot) and /vel(from userinterface) topics are subscribed. */
+    /* /base_scan topic is subscribed. */
     ros::Subscriber sub = nh.subscribe("/base_scan", 1000, baseCallback);
-    ros::Subscriber sub2 = nh.subscribe("/vel", 1000, velCallback); /// addd
+
+    /* service is initiated with callback function. */
+    vel_service = nh.advertiseService("/service", serviceCallback);
 
     /* publishing to the robot's /cmd_vel topic */
     pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
--- src/00_userinterface.cpp
+++ src/00_userinterface.cpp
@@ -2,7 +2,6 @@
 #include "sensor_msgs/LaserScan.h"
 #include "geometry_msgs/Twist.h"
 #include "std_srvs/Empty.h"
-#include "second_assignment/vel.h"
 #include "second_assignment/UserInput.h"
 
 ros::Publisher pub;
@@ -11,18 +10,15 @@ ros::ServiceClient client_userinp;
 
 char c;
 
-second_assignment::vel vel;
-
 int main(int argc, char **argv)
 {
     /* classic entrance. init and node handler. */
     ros::init(argc, argv, "userinterface");
     ros::NodeHandle nh;
 
-    /* reset and command clients are initiated, /vel topic is initiated to send to controller */
+    /* reset and command clients are initiated */
     client_reset = nh.serviceClient<std_srvs::Empty>("/reset_positions");
     client_userinp = nh.serviceClient<second_assignment::UserInput>("/service");
-    pub = nh.advertise<second_assignment::vel>("/vel", 1);
 
     /* some more necessary declarations */
     std_srvs::Empty res_ser;
@@ -30,29 +26,29 @@ int main(int argc, char **argv)
 
     while (ros::ok())
     {
-        printf("Enter a for decrease speed, d for speed increase and r for reset position:");
-        while ((c = getchar()) != '\n' && c != EOF)
+        printf("Enter z to decrease, x to increase speead, r to reset: ");
+        system ("/bin/stty raw");
+        while (c = getchar())
         {
-            getchar(); // this extra getchar is to get rid of '\n' at the end. User hits 'enter' after the input command and creates is non-deliberately.
+            //getchar(); // this extra getchar is to get rid of '\n' at the end. User hits 'enter' after the input command and creates is non-deliberately.
             switch (c) // source: https://stackoverflow.com/questions/25374573/c-problems-with-using-getchar-and-switch-case-to-get-user-input-for-a-main-menu
             {
-            case 'a':
-            case 'd':
-            case 'A':
-            case 'D':
+            case 'z':
+            case 'Z':
+            case 'x':
+            case 'X':
                 /* increase or decrease command */
                 system("clear");
-                printf("Enter a for decrease speed, d for speed increase and r for reset position:\n");
+                printf("Enter z to decrease, x to increase speead, r to reset: ");
                 user_ser.request.userinp = c; // request is set
                 client_userinp.waitForExistence();
                 client_userinp.call(user_ser);          // request is sent
-                vel.velnow = user_ser.response.uservel; // response is taken and assigned to topic messages field
-                pub.publish(vel);                       // response is published to /vel to controller
                 break;
             case 'r':
+            case 'R':
                 /* reset command */
                 system("clear");
-                printf("Enter a for decrease speed, d for speed increase and r for reset position:\n");
+                printf("Enter z to decrease, x to increase speead, r to reset: ");
                 client_reset.waitForExistence();
                 client_reset.call(res_ser);
                 break;
@@ -60,12 +56,11 @@ int main(int argc, char **argv)
             default:
                 /* other inputs are ignored */
                 system("clear");
-                printf("Please enter a for decrease speed, d for speed increase and r for reset position:\n");
+                printf("Enter z to decrease, x to increase speead, r to reset: ");
                 break;
             }
         }
     }
-
-    ros::spin();
+    system ("/bin/stty cooked");
     return 0;
 }
