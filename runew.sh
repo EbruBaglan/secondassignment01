3b6f1b607d3dee9fa247ed86510ef332e6d808aa
dolunaysuratusagi <baglanebru@gmail.com>
2021-12-18 08:37:26 +0100

    FINALMENTE!!!

--- runew.sh
+++ runew.sh
@@ -1,4 +1,3 @@
-gnome-terminal --tab --title="tab 0" -- rosrun stage_ros stageros $(rospack find second_assignment)/world/my_world.world  
-gnome-terminal --tab --title="tab 1" -- rosrun second_assignment service  
-gnome-terminal --tab --title="tab 2" -- rosrun second_assignment controller
-gnome-terminal --tab --title="tab 3" -- rosrun second_assignment userinterface
+gnome-terminal --tab --title="tab 0" -- rosrun stage_ros stageros $(rospack find second_assignment)/world/my_world.world    
+gnome-terminal --tab --title="tab 1" -- rosrun second_assignment controller
+gnome-terminal --tab --title="tab 2" -- rosrun second_assignment userinterface
